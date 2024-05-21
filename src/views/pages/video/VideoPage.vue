71caafdad9bf0f68d2b8d434920d8116069f3afa
chann vuthy <channvuthyit@gmail.com>
2024-05-21 11:25:30 +0700

    Add scroll to top

--- src/views/pages/video/VideoPage.vue
+++ src/views/pages/video/VideoPage.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="p-5 w-full overflow-y-scroll" :style="{ height: `${windowHeight - 63}px` }" @scroll="handleScroll">
+    <div class="p-5 w-full overflow-y-scroll" :style="{ height: `${windowHeight - 63}px` }" @scroll="handleScroll"
+        ref="scrolltop">
         <Search />
         <div v-if="loading">
             <div>
@@ -12,35 +13,52 @@
             <VideoCollection />
             <VideoCouse />
         </div>
+
+        <!-- Scroll to top -->
+        <div class="fixed right-5 bottom-5 rounded-full flex flex-col items-center justify-center cursor-pointer"
+            v-if="isScrollTop" @click="toTop">
+            <div class="bg-red-500 rounded-full w-10 h-10 flex items-center justify-center">
+                <ArrowUpIcon fill="#FFF" />
+            </div>
+            <p class="text-sm uppercase font-default-bold text-red-500 mt-2">Scroll to Top</p>
+        </div>
     </div>
 </template>
+
 <script setup>
 import Search from './partials/filter/Search.vue';
 import { useStore } from 'vuex';
 import { computed, onMounted, ref, onUnmounted } from 'vue';
-import PackageLoading from './partials/loading/PackageLoading.vue'
-import VideoCollection from './partials/packages/VideoCollection.vue'
-import VideoCouse from "./course/VideoCourse.vue"
+import PackageLoading from './partials/loading/PackageLoading.vue';
+import VideoCollection from './partials/packages/VideoCollection.vue';
+import VideoCouse from "./course/VideoCourse.vue";
+import ArrowUpIcon from "@components/icons/commons/ArrowUpIcon.vue";
 
 const store = useStore();
 const loading = computed(() => store.state.video.loading);
 const filters = computed(() => store.state.video.filters);
 const p = ref(1);
 const isNoMoreData = ref(false);
+const isScrollTop = ref(false);
+const scrolltop = ref(null);
 
-/**
- * Handles the scroll event and fetches more videos when reaching the bottom.
- *
- * @param {Event} event - The scroll event object.
- * @return {Promise<void>} A promise that resolves when the videos are fetched.
- */
+const toTop = () => {
+    scrolltop.value.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+};
+
+const handleScrollToTop = (scrollHeight) => {
+    isScrollTop.value = scrollHeight > windowHeight.value;
+};
+
 const handleScroll = async (event) => {
     const isBottom = event.target.scrollTop + event.target.clientHeight >= event.target.scrollHeight;
 
-    if (isBottom) {
-        if (isNoMoreData.value)
-            return;
+    handleScrollToTop(event.target.scrollTop);
 
+    if (isBottom && !isNoMoreData.value) {
         p.value++;
         await store.dispatch('video/fetchVideos', { p: p.value, ...filters.value })
             .then((data) => {
@@ -49,8 +67,7 @@ const handleScroll = async (event) => {
                 }
             });
     }
-}
-
+};
 
 onMounted(async () => {
     await store.dispatch('video/fetchVideos');
@@ -69,5 +86,4 @@ onMounted(() => {
 onUnmounted(() => {
     window.removeEventListener("resize", updateHeight);
 });
-
 </script>
